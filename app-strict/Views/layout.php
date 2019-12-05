4f390bbb3f3097d063507ebf5db262915ecca4f9
john-betong <ajrutherford@gmail.com>
2019-12-05 20:21:10 +0700

    added PlayGround

--- app-strict/Views/layout.php
+++ app-strict/Views/layout.php
@@ -27,36 +27,35 @@
   </div>
 
   <div class="article tac">
-    
     <i class="flr">
         <a href="https://github.com/codeigniter4projects/playground">
             GitHub PlayGround
         </a>
     </i>
 
-    <div class="fss"
+    <div class="fss">
       <?php echo $this->renderSection('content') ?>
     </div>    
 
     <p> &nbsp; </p>
 
     <div class="tac">
-        <dl class="dib tal bd1">
+      <dl class="dib tal bd1">
         <dt class="bgd"> Stats: </dt>
         <dd> &nbsp; </dd>
         <dd> Page rendered in {elapsed_time} seconds.</dd>
-        
+           
         <dd>
-            <p class="text-center">PHP <?= phpversion() ?></p>
+          <p class="text-center">PHP <?= phpversion() ?></p>
         </dd>
         <dd class="col">
-            <p class="text-right">Environment: <?= ENVIRONMENT ?></p>
+          <p class="text-right">Environment: <?= ENVIRONMENT ?></p>
         </dd>
       </dl>
-    <div><!-- class="tac" -->  
-    <p> <br><br><br><br><br> </p>
-    <p> <br><br><br><br><br> </p>
-    <p> <br><br><br><br><br> </p>
+    </div><!-- class="tac" -->  
+
+    <!-- KLUDGE -->        
+      <p> <br><br><br><br><br> </p>
   </div><!-- class="article tac" -->
 
   <div class="footer tac p42">
@@ -64,12 +63,13 @@
       # echo $footer; 
     ?> 
    </div>
-   
+
+</body>
+</html><?php 
+/*
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-</body>
-</html>
+*/
